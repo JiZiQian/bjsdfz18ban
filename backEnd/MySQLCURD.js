d6cf8ebeab741b003bfe35353a1cdd3eb6796888
JiZiQian <jzq20015005@outlook.com>
2023-03-23 18:13:53 +0800

    Added MySQL

---
+++ backEnd/MySQLCURD.js
@@ -0,0 +1,20 @@
+var mysql=require('mysql');
+var connection;
+exports.init=function(hst,usr,pswd,dtbs){
+    connection=mysql.createConnection({
+        host:hst,user:usr,password:pswd,database:dtbs
+    });
+    connection.connect();
+}
+exports.query=function(table,add,callback){
+    connection.query('SELECT * FROM '+table+' '+add,callback);
+};
+exports.insert=function(table,title,content,callback){
+    connection.query('INSERT INTO '+table+' ('+title+') VALUES ('+content+')',callback);
+};
+exports.update=function(table,content,where,callback){
+    connection.query('UPDATE '+table+' SET '+content+' '+add,callback);
+};
+exports.delete=function(table,where,callback){
+    connection.query('DELETE '+table+' '+where,callback);
+};
--- backEnd/server.js
+++ backEnd/server.js
@@ -1,6 +1,8 @@
 var http=require("http");
 var querystring = require('querystring');
 var fs=require("fs");
+var mysql=require("./MySQLCURD");
+mysql.init('localhost','root','jzq20100505','IssuesBoard');
 function replace(str,fd,to){
     while(str.indexOf(fd)!=-1){
         str=str.substr(0,str.indexOf(fd))+to+str.substr(str.indexOf(fd)+fd.length);
@@ -16,27 +18,36 @@ http.createServer(function(req,res){
         post=querystring.parse(post);
         res.writeHead(200,{"Content-Type":"text/html;charset=utf-8","Access-Control-Allow-Origin":"*"});
         if(post.type=="upload"){
-            var data=fs.readFileSync("issues.txt").toString();
             var str=post.text;
+            console.log("upload");
             str=replace(str,"<","&lt;");
             str=replace(str,">","&gt;");
             str=replace(str,"\n","<br/>");
             str=replace(str,"\'","&#39;");
             str=replace(str,"\"","&quot;");
             console.log(str);
-            if(data.indexOf(str)==-1){
-                data=data+str+"<br/><br/>";
-            }
-            fs.writeFile("issues.txt",data,function(err){
-                if(err) return console.error(err);
+            mysql.insert('Issues','id,issue','0,\"'+str+'\"',function(err,res){
+                if(err){
+                    console.log("error "+err.message);
+                    return;
+                }
+                console.log(res);
             });
             res.end();
         }
         if(post.type=="getIssues"){
             console.log("getIssues");
-            var data=fs.readFileSync("issues.txt").toString();
-            console.log(data);
-            res.end(data);
+            var data="";
+            var x=mysql.query('Issues','',function(err,result){
+                if(err){
+                    console.log("error "+err.message);
+                    return;
+                }
+                for(var i=0;i<result.length;i++){
+                    data+='<div id=\"texts\" style=\"background-color:#f1f1f1;border:10px solid #ffffff;padding:1%;border-radius:25px;text-align:left\">'+result[i].issue+'</div>'
+                }
+                res.end(data);
+            })
         }
     });
 }).listen(1801);//issue board
