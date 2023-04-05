7ffbaee82c689d265d8846b4f637594d9ac37507
bjsdfz17ban <wchengk09@163.com>
2023-04-05 13:38:43 +0800

    debug

--- user.js
+++ user.js
@@ -1,9 +1,9 @@
 let ckie=document.cookie.split(";");
 function show(){
-    document.getElementById("user").innerHTML="<button id=\"userName\" onclick=\"logOut()\" onmouseout=\"hide()\" style=\"height:25px;width:60px;\">登出</button>";
+    document.getElementById("user").innerHTML="<button id=\"userName\" onclick=\"logOut()\" onmouseout=\"hide()\" style=\"height:auto;width:auto;\">登出</button>";
 }
 function hide(){
-    document.getElementById("user").innerHTML="<h3 id=\"userName\" onmouseover=\"show()\"></h3>";
+    document.getElementById("user").innerHTML="<p id=\"userName\" onmouseover=\"show()\" style=\"font-style:italic;\"></p>";
     document.getElementById("userName").innerHTML=ckie[0].substr(ckie[0].indexOf("=")+1);
 }
 function logOut(){
@@ -14,7 +14,7 @@ function logOut(){
 function userInit(){
     console.log(ckie);
     if(ckie.length==1){
-        document.getElementById("user").innerHTML="<a href=\"https://jiziqian.github.io/signIn\" id=\"userName\">登录</a>";
+        document.getElementById("user").innerHTML="<a href=\"https://jiziqian.github.io/bjsdfz18ban/signIn\" id=\"userName\" style=\"text-decoration:none;color:black;border:2px solid black;border-radius:10px;padding:5px;\">登录</a>";
     }
     else{
         hide();
