f31ff679d481eb3b5f7a8d15c845a78931d6489c
bjsdfz17ban <wchengk09@163.com>
2023-04-05 20:05:36 +0800

    debug

--- backEnd/issue.js
+++ backEnd/issue.js
@@ -7,7 +7,7 @@ exports.upload=function(post,res,replace){
     console.log("upload");
     str=replace(str,"<","&lt;");
     str=replace(str,">","&gt;");
-    str=replace(str,"\n","<br/>\n");
+    str=replace(str,"\n","\n<br/>");
     str=replace(str,"\'","&#39;");
     str=replace(str,"\"","&quot;");
     console.log(str);
