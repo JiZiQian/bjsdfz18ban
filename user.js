d1024c96bf5fb18dde8d6d16853945a779ae284f
bjsdfz17ban <wchengk09@163.com>
2023-04-05 11:12:39 +0800

    add logOut button

--- user.js
+++ user.js
@@ -9,13 +9,15 @@ function logOut(){
     document.cookie="uid=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
     window.location.reload();
 }
-let ckie=document.cookie.split(";");
-console.log(ckie);
-if(ckie.length==0){
-    document.getElementById("user").innerHTML="<a href=\"https://jiziqian.github.io/signIn\"></a>";
-}
-else{
-    document.getElementById("user").innerHTML="<p id=\"userName\" onmouseover=\"show();\"></p><button onmouseout=\"hide()\" class=\"logOut\" id=\"logOut\" onclick=\"logOut()\">登出</button>";
-    hide();
-    document.getElementById("userName").innerHTML=ckie[0].substr(ckie[0].indexOf("=")+1);
+function userInit(){
+    let ckie=document.cookie.split(";");
+    console.log(ckie);
+    if(ckie.length==0){
+        document.getElementById("user").innerHTML="<a href=\"https://jiziqian.github.io/signIn\"></a>";
+    }
+    else{
+        document.getElementById("user").innerHTML="<p id=\"userName\" onmouseover=\"show();\"></p><button onmouseout=\"hide()\" class=\"logOut\" id=\"logOut\" onclick=\"logOut()\">登出</button>";
+        hide();
+        document.getElementById("userName").innerHTML=ckie[0].substr(ckie[0].indexOf("=")+1);
+    }
 }
