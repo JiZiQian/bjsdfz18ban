ee7c6c8969106bc25db7c05974131a0f8cf5617c
bjsdfz17ban <wchengk09@163.com>
2023-04-13 20:13:21 +0800

    fix

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function init(){
     let ajaxHttp;
     if(window.XMLHttpRequest) ajaxHttp=new XMLHttpRequest();
     else ajaxHttp=new ActiveXObject("Microsoft.XMLHTTP");
-    ajaxHttp.open("GET","https://bjsdfz17ban.github.io/domain/18666.txt?t="+Math.random(),false);
+    ajaxHttp.open("GET","https://wchengk09.github.io/domain/18666.txt?t="+Math.random(),false);
     ajaxHttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     ajaxHttp.send();
     httpBackEnd=ajaxHttp.responseText;
