1e21bbb2410c489b0abb8e0a02ccc75c00255d39
bjsdfz17ban <wchengk09@163.com>
2023-05-24 12:46:11 +0800

    debug

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ function init(){
     let ajaxHttp;
     if(window.XMLHttpRequest) ajaxHttp=new XMLHttpRequest();
     else ajaxHttp=new ActiveXObject("Microsoft.XMLHTTP");
-    ajaxHttp.onreadystatechange=function(){
-        httpBackEnd=ajaxHttp.responseText;
-    }
-    ajaxHttp.open("GET","https://wchengk09.github.io/domain/18666.txt?t="+Math.random(),true);
+    ajaxHttp.open("GET","https://wchengk09.github.io/domain/18666.txt?t="+Math.random(),false);
     ajaxHttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     ajaxHttp.send();
+    httpBackEnd=ajaxHttp.responseText;
 }
